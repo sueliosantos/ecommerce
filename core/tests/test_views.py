018c3aaffdcc627daaf4b06fdde3668a8effe40b
Suelio Santos <sueliosantos@gmail.com>
2018-12-12 21:16:52 -0300

    teste

---
+++ core/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, Client
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
+
